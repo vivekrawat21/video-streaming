5eb459b82ce2f050fef61faa63fc390ce4b7e8a1
vivekra21 <vivekrwt2111@gmail.com>
2024-05-04 01:13:43 +0530

    ignored the nodemodules

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import {v4 as uuidv4} from "uuid";
+import path from "path";
+
+
+const app = express();
+//multer middleware
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads")
+    },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
+        }
+
+    });
+  
+
+
+app.use(cors(
+    {
+        origin: ["http://localhost:3000","http://localhost:5173"],
+        credentials: true
+    }
+));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+
+})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/uploads",express.static("uploads"));
+app.use(multer().single("file"));
+
+app.get('/', (req, res) => {
+    res.json({message: "Hello brother"});
+})
+
+app.listen(8000, () => {
+    console.log("App is listening at port 3000");
+})
